29b267f76273d913f075c4840824dd849b0f46f3
dirkrydvan <36986019+dirkrydvan@users.noreply.github.com>
2018-05-24 13:30:23 +0200

    Update create-basic-auth-from-secret.sh

--- httpd-pre-init/create-basic-auth-from-secret.sh
+++ httpd-pre-init/create-basic-auth-from-secret.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo " pre init script"
 ### skript zum erstellen .htacces und .htpasswd
-echo 'AuthType Basic' > ../.htaccess
-echo 'AuthName '$(cat /tmp/phpmyadmin-basicauth-secret/phpmyadmin-authname) >> ../.htaccess
-echo 'AuthUserFile /tmp/.htpasswd' >> ../.htaccess
-echo 'Require user ' $(cat /tmp/phpmyadmin-basicauth-secret/phpmyadmin-username) >> ../.htaccess
+echo 'AuthType Basic' > $HOME/.htaccess
+echo 'AuthName '$(cat /tmp/phpmyadmin-basicauth-secret/phpmyadmin-authname) >>  $HOME/.htaccess
+echo 'AuthUserFile /tmp/.htpasswd' >>  $HOME/.htaccess
+echo 'Require user ' $(cat /tmp/phpmyadmin-basicauth-secret/phpmyadmin-username) >>  $HOME/.htaccess
 htpasswd -b -c /tmp/.htpasswd $(cat /tmp/phpmyadmin-basicauth-secret/phpmyadmin-username) $(cat /tmp/phpmyadmin-basicauth-secret/phpmyadmin-password)
