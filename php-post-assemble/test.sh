f7dd42cbf27ba1c3be78122d0364e07a2b673aa2
dirkrydvan <36986019+dirkrydvan@users.noreply.github.com>
2018-06-07 13:17:23 +0200

    Create test.sh

---
+++ php-post-assemble/test.sh
@@ -0,0 +1,2 @@
+echo 'This content was replaced by php-pots-assamble script.' > ${HTTPD_APP_ROOT}/src/index2.html
+echo 'This content was replaced by php-pots-assamble script2.' > ${HTTPD_APP_ROOT}/src/index2.html
