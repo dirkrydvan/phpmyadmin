6f8341724944ed64c60a4568bbf32d1b2bb9a16e
Dirk Rydvan <36986019+dirkrydvan@users.noreply.github.com>
2018-06-27 13:25:51 +0200

    Update test.sh

--- php-post-assemble/test.sh
+++ php-post-assemble/test.sh
@@ -1,2 +1,1 @@
-echo 'This content was replaced by php-post-assamble script.' > ${HTTPD_APP_ROOT}/src/index2.html
-echo 'This content was replaced by php-post-assamble script2.' > /tmp/index2.html
+echo 'This content was replaced by php-post-assamble script.' > /tmp/index.html
